52f43b16c165f876d916db6c633e4a93f5bb1002
RYzZzEN007 <subashchoudhary12121@gmail.com>
2025-09-13 00:50:43 +0530

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { LanguageProvider } from "@/hooks/useLanguage";
+import { Analytics } from "@vercel/analytics/react";
+
 import App from './App.tsx'
 import './index.css'
 
@@ -16,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
         <BrowserRouter>
           <App />
           <Toaster />
+          <Analytics />
         </BrowserRouter>
       </TooltipProvider>
     </LanguageProvider>
